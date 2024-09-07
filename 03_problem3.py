7f72f0aca6ab5bc071ffe8b5a1f55c82286481d1
uvpatel <uvpatel7271@gmail.com>
2024-09-07 10:58:54 -0700

    Delete 03_problem3.py

--- 03_problem3.py
+++
@@ -1,5 +0,0 @@
-'''A spam comment is defined as a text containing following keywords:
-“Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program
-to detect these spams.'''
-
-spam = {}
